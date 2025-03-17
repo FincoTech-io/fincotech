9c2b5921157fef22626ab7828d332b5a21ccb5f6
ASGUM <asher@fincotech.io>
2025-03-17 01:33:07 -0700

    remove swagger

--- src/app/api/swagger/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { getSwaggerSpec } from '../../../utils/swagger';
-
-export async function GET() {
-  const spec = getSwaggerSpec();
-  return NextResponse.json(spec);
-} 
